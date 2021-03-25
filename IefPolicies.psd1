ec301a52b55efe3191093e88cdca8425776db40a
Marius Rochon <mrochon@microsoft.com>
2021-03-25 12:18:43 -0700

    New code

---
+++ IefPolicies.psd1
@@ -0,0 +1,72 @@
+@{
+
+# Script module or binary module file associated with this manifest.
+RootModule = 'IefPolicies.psm1'
+
+# Version number of this module.
+ModuleVersion = '1.0.0'
+
+# Supported PSEditions
+# CompatiblePSEditions = @()
+
+# ID used to uniquely identify this module
+GUID = '037d0382-a043-46a7-b420-a8eae3f4734b'
+
+# Author of this module
+Author = 'Marius Rochon'
+
+# Company or vendor of this module
+CompanyName = 'Microsoft'
+
+# Copyright statement for this module
+Copyright = '(c) Marius Rochon. All rights reserved.'
+
+# Description of the functionality provided by this module
+Description = 'Intelligently upload and download xml IEF policies used for custom journeys in Azure AD B2C'
+
+# Modules that must be imported into the global environment prior to importing this module
+RequiredModules = @(
+    @{ ModuleName = 'AzureADPreview'; ModuleVersion = '2.0.2.129' }
+)
+
+# Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
+FunctionsToExport = @(
+    'Export-IEFPolicies'
+    'Import-IEFPolicies'
+)
+
+# Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
+PrivateData = @{
+
+    PSData = @{
+
+        # Tags applied to this module. These help with module discovery in online galleries.
+        # Tags = @()
+
+        # A URL to the license for this module.
+        # LicenseUri = ''
+
+        # A URL to the main website for this project.
+        # ProjectUri = ''
+
+        # A URL to an icon representing this module.
+        # IconUri = ''
+
+        # ReleaseNotes of this module
+        # ReleaseNotes = ''
+
+        # Prerelease string of this module
+        # Prerelease = ''
+
+        # Flag to indicate whether the module requires explicit user acceptance for install/update/save
+        # RequireLicenseAcceptance = $false
+
+        # External dependent modules of this module
+        # ExternalModuleDependencies = @()
+
+    } # End of PSData hashtable
+
+} # End of PrivateData hashtable
+
+}
+
---
+++ IefPolicies.psm1
@@ -0,0 +1,295 @@
+﻿function Import-IEFPolicies {
+    <#
+    .SYNOPSIS
+    Short description
+    
+    .DESCRIPTION
+    Long description
+    
+    .PARAMETER sourceDirectory
+    Parameter description
+    
+    .PARAMETER configurationFilePath
+    Parameter description
+    
+    .PARAMETER updatedSourceDirectory
+    Parameter description
+    
+    .PARAMETER prefix
+    Parameter description
+    
+    .PARAMETER generateOnly
+    Parameter description
+    
+    .EXAMPLE
+        PS C:\> Import-IEFPolicies
+
+        Upload policies from the current work directory using conf.json file for configuration data if it exists.
+    
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param(
+        #[Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$sourceDirectory = '.\',
+
+        [ValidateNotNullOrEmpty()]
+        [string]$configurationFilePath = '.\conf.json',
+
+        [ValidateNotNullOrEmpty()]
+        [string]$updatedSourceDirectory = '.\debug\',
+
+        [ValidateNotNullOrEmpty()]
+        [string]$prefix,
+
+        [ValidateNotNullOrEmpty()]
+        [switch]$generateOnly
+
+    )
+
+    if ($sourceDirectory.EndsWith('\')) {
+        $sourceDirectory = $sourceDirectory + '*' 
+    } else {
+        if (-Not $sourceDirectory.EndsWith('\*')) { 
+            $sourceDirectory = $sourceDirectory + '\*' 
+        }
+    }
+
+    # upload policies whose base id is given
+    function Upload-Children($baseId) {
+        foreach($p in $policyList) {
+            if ($p.BaseId -eq $baseId) {
+                # Skip unchanged files
+                #outFile = ""
+                if (-not ([string]::IsNullOrEmpty($updatedSourceDirectory))) {
+                    if(!(Test-Path -Path $updatedSourceDirectory )){
+                        New-Item -ItemType directory -Path $updatedSourceDirectory
+                        Write-Host "Updated source folder created"
+                    }
+                    if (-not $updatedSourceDirectory.EndsWith("\")) {
+                        $updatedSourceDirectory = $updatedSourceDirectory + "\"
+                    }
+                    $envUpdatedDir = '{0}{1}' -f $updatedSourceDirectory, $b2c.TenantDomain
+                    if(!(Test-Path -Path $envUpdatedDir)){
+                        New-Item -ItemType directory -Path $envUpdatedDir
+                        Write-Host "  Updated source folder created for " + $b2c.TenantDomain
+                    }
+                    $outFile = '{0}\{1}' -f $envUpdatedDir, $p.Source
+                    if (Test-Path $outFile) {
+                        if ($p.LastWrite.Ticks -le (Get-Item $outFile).LastWriteTime.Ticks) {
+                            "{0}: is up to date" -f $p.Id
+                            Upload-Children $p.Id
+                            continue;
+                        }
+                    }
+                }
+                $msg = "{0}: uploading" -f $p.Id
+                Write-Host $msg  -ForegroundColor Green 
+                # Replace tenant id but only if already there. It messes up xml formatting
+                $xml = [xml] $p.Body
+                $xml.PreserveWhitespace = $true
+                try {
+                    $xml.TrustFrameworkPolicy.TenantObjectId = $b2c.TenantId.ToString()
+                    $policy = $xml.OuterXml
+                } catch {
+                    # tenantId not used
+                    $policy = $p.Body
+                    #$xmlAtt = $xml.CreateAttribute("TenantObjectId")
+                    #$xmlAtt.Value = $b2c.TenantId.ToString()
+                    #$txt = $xml.TrustFrameworkPolicy.Attributes.Append($xmlAtt)
+                }
+                $policy = $policy -replace "yourtenant", $b2cName 
+                $policy = $policy -replace "ProxyIdentityExperienceFrameworkAppId", $iefProxy.AppId
+                $policy = $policy -replace "IdentityExperienceFrameworkAppId", $iefRes.AppId
+                $policy = $policy.Replace('PolicyId="B2C_1A_', 'PolicyId="B2C_1A_{0}' -f $prefix)
+                $policy = $policy.Replace('/B2C_1A_', '/B2C_1A_{0}' -f $prefix)
+                $policy = $policy.Replace('<PolicyId>B2C_1A_', '<PolicyId>B2C_1A_{0}' -f $prefix)
+
+                # replace other placeholders, e.g. {MyRest} with http://restfunc.com. Note replacement string must be in {}
+                if ($conf -ne $null) {
+                    $special = @('IdentityExperienceFrameworkAppId', 'ProxyIdentityExperienceFrameworkAppId', 'PolicyPrefix')
+                    foreach($memb in Get-Member -InputObject $conf -MemberType NoteProperty) {
+                        if ($memb.MemberType -eq 'NoteProperty') {
+                            if ($special.Contains($memb.Name)) { continue }
+                            $repl = "{{{0}}}" -f $memb.Name
+                            $policy = $policy.Replace($repl, $memb.Definition.Split('=')[1])
+                        }
+                    }
+                }
+
+                $policyId = $p.Id.Replace('_1A_', '_1A_{0}' -f $prefix)
+                $isOK = $true
+                if (-not $generateOnly) {
+                $exists = $true
+                    try {
+                        $curr = Get-AzureADMSTrustFrameworkPolicy -Id $policyId
+                    } catch {
+                        $exists = $false
+                    }
+                    try {
+                        if ($exists) {
+                            "Replacing"
+                            Set-AzureADMSTrustFrameworkPolicy -Content ($policy | Out-String) -Id $policyId | Out-Null
+                        } else {
+                            "New journey"
+                            New-AzureADMSTrustFrameworkPolicy -Content ($policy | Out-String) | Out-Null
+                        }
+                    } catch {
+                        $isOk = $false
+                        $_
+                        if(-Not $exists) {
+                            "Use https://b2ciefsetup.azurewebsites.net to ensure the tenant is setup for IEF"
+                        }
+                        "Ending upload"
+                    }
+                }
+
+                if ($isOK) {
+                    out-file -FilePath $outFile -inputobject $policy
+                    Upload-Children $p.Id
+                }
+            }
+        }
+    }
+
+    # get current tenant data
+    $b2c = Get-AzureADCurrentSessionInfo -ErrorAction stop
+    $b2cName = $b2c.TenantDomain.Split('.')[0]
+    
+    $iefRes = Get-AzureADApplication -Filter "DisplayName eq 'IdentityExperienceFramework'"
+    $iefProxy = Get-AzureADApplication -Filter "DisplayName eq 'ProxyIdentityExperienceFramework'"
+
+    # load originals
+    $files = Get-Childitem -Path $sourceDirectory -Include *.xml
+    $policyList = @()
+    foreach($policyFile in $files) {
+        $policy = Get-Content $policyFile
+        $xml = [xml] $policy
+        $policyList= $policyList + @(@{ Id = $xml.TrustFrameworkPolicy.PolicyId; BaseId = $xml.TrustFrameworkPolicy.BasePolicy.PolicyId; Body = $policy; Source= $policyFile.Name; LastWrite = $policyFile.LastWriteTime })
+    }
+    "Source policies:"
+    foreach($p in $policyList) {
+        "Id: {0}; Base:{1}" -f $p.Id, $p.BaseId
+    }
+
+    if (-not ([string]::IsNullOrEmpty($configurationFilePath))) {
+        $conf = Get-Content -Path $configurationFilePath -ErrorAction Continue | Out-String | ConvertFrom-Json
+        if ([string]::IsNullOrEmpty($prefix)){ $prefix = $conf.Prefix }
+    } else {
+        $conf = $null
+    }
+
+    # now start the upload process making sure you start with the base (base id == null)
+    Upload-Children($null)
+}
+
+# Creates a json object with typical settings needed by
+# the Upload-IEFPolicies function.
+function Get-IEFSettings {
+    [CmdletBinding()]
+    param(
+        [ValidateNotNullOrEmpty()]
+        [string]$policyPrefix
+    )
+
+    $iefAppName = "IdentityExperienceFramework"
+    if(!($iefApp = Get-AzureADApplication -Filter "DisplayName eq '$($iefAppName)'"  -ErrorAction SilentlyContinue))
+    {
+        throw "Not found " + $iefAppName
+    } else {
+        if ($iefApp.PublicClient) {
+            Write-Error "IdentityExperienceFramework must be defined as a confidential client (web app)"
+        }
+    }
+
+    $iefProxyAppName = "ProxyIdentityExperienceFramework"
+    if(!($iefProxyApp = Get-AzureADApplication -Filter "DisplayName eq '$($iefProxyAppName)'"  -ErrorAction SilentlyContinue))
+    {
+        throw "Not found " + $iefProxyAppName
+    } else {
+        if (-not $iefProxyApp.PublicClient) {
+            Write-Error "ProxyIdentityExperienceFramework must be defined as a public client"
+        }
+        $iefOK = $signInOk = $False
+        foreach($r in $iefProxyApp.RequiredResourceAccess) {
+            if ($r.ResourceAppId -eq $iefApp.AppId) { $iefOk = $true }
+            if ($r.ResourceAppId -eq '00000002-0000-0000-c000-000000000000') { $signInOk = $true }
+        }
+        if ((-not $iefOK) -or (-not $signInOk)) {
+            Write-Error 'ProxyIdentityExperienceFramework is not permissioned to use the IdentityExperienceFramework app (it must be consented as well)'
+        } 
+    }
+
+    $envs = @()
+    $envs += @{ 
+        IdentityExperienceFrameworkAppId = $iefApp.AppId;
+        ProxyIdentityExperienceFrameworkAppId = $iefProxyApp.AppId;
+        PolicyPrefix = $policyPrefix  }
+    $envs | ConvertTo-Json
+
+    <#
+     # 
+    $iefAppName = "IdentityExperienceFramework"
+    if(!($iefApp = Get-AzureADApplication -Filter "DisplayName eq '$($iefAppName)'"  -ErrorAction SilentlyContinue))
+    {
+        Write-Host "Creating " $iefAppName
+        $myApp = New-AzureADApplication -DisplayName $iefAppName   
+    }
+    $iefProxyAppName = "ProxyIdentityExperienceFramework"
+    if(!($iefProxyApp = Get-AzureADApplication -Filter "DisplayName eq '$($iefProxyAppName)'"  -ErrorAction SilentlyContinue))
+    {
+        Write-Host "Creating " $iefAppName
+        $myApp = New-AzureADApplication -DisplayName $iefAppName   
+    }
+    #>
+}
+
+function Export-IEFPolicies {
+<#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER prefix
+    Parameter description
+
+    .PARAMETER destinationPath
+    Parameter description
+
+    .EXAMPLE
+        PS C:\> Export-IEFPolicies -prefix V10
+
+        Download IEF policies with names starting with 'B2C_1A_V10'
+
+    .NOTES
+    General notes
+#>
+    [CmdletBinding()]
+    param(
+        #[Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$prefix,
+
+        #[Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$destinationPath
+    )
+    if ([string]::IsNullOrEmpty($desinationPath)) {
+        $destinationPath = ".\"
+    }
+    $null = Get-AzureADCurrentSessionInfo -ErrorAction Stop
+
+    if (-Not $destinationPath.EndsWith('\')) {
+        $destinationPath = $destinationPath + '\' 
+    }
+
+    $prefix = "B2C_1A_" + $prefix
+    foreach($policy in Get-AzureADMSTrustFrameworkPolicy | Where-Object {($_.Id).startsWith($prefix)}) {
+        $fileName = "{0}\{1}.xml" -f $destinationPath, $policy.Id
+        Get-AzureADMSTrustFrameworkPolicy -Id $policy.Id >> $fileName
+    }
+}
